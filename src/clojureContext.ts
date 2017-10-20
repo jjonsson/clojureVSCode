674ecb9516c9092c67631aba15ffe803c8720a53
Jay Jonsson <jjonsson@cs.hmc.edu>
2017-10-19 19:05:20 -0500

    Fixed bugs ClojureContextResolver bugs. All Tests now pass.

--- src/clojureContext.ts
+++ src/clojureContext.ts
@@ -6,7 +6,7 @@ export enum ClojureContext {
 
 interface ContextRange {
     type: ClojureContext;
-    range: [number, number];
+    range: [number, number]; // [start, end)
 }
 
 const CHARACTERS = [
@@ -24,82 +24,96 @@ export class ClojureContextResolver {
     private contexts: Array<ContextRange>;
     private document: string;
 
+    // scan forward until a matching pair of quotes is found or the document ends
+    private findStringEnd(offset: number): number {
+        let result: number;
+
+        for (result = offset; result < this.document.length; result++) {
+            if (this.document[result] === '\\') { // Java escape character
+                result++; // skip one character
+            }
+            else if (this.document[result] === '"') {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    // scan forward until a new line is found or the document ends
+    private findCommentEnd(offset: number): number {
+        let result: number;
+
+        for (result = offset; result < this.document.length; result++) {
+            if (this.document[result] === '\n') {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private findCharacterLiteralEnd(offset: number) {
+        // match the next few characters with a given word
+        const lookahead = (match: string) => {
+            for (let i = 0; i < match.length; i++) {
+                if (offset + i >= this.document.length) { // we've gone off the end of the document
+                    return false;
+                }
+                if (this.document[offset + i] !== match[i]) { // this character doesn't match
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        if (lookahead('u')) { // unicode character of the form \uNNNN
+            offset += 4;
+        }
+        else if (lookahead('o')) { // octal character of the form \oNNN
+            offset += 3;
+        }
+        else {
+            let charCodeMatched = false;
+            for (let charCode of CHARACTERS) { // match characters like \tab or \newline
+                if (lookahead(charCode)) {
+                    offset += charCode.length;
+                    charCodeMatched = true;
+                    break;
+                }
+            }
+            if (!charCodeMatched) { // this is just a single character
+                offset++;
+            }
+        }
+
+        return offset;
+    }
+
     // parse a document for strings and comments (this is where the hard work is done)
     public constructor(document: string) {
         this.contexts = [];
         this.document = document;
 
-        let inString = false;
-        let stringStartIndex: number = null;
-
         for (let offset = 0; offset < document.length; offset++) {
-            // match the next few characters with a given word
-            const lookahead = (match: string) => {
-                for (let i = 0; i < match.length; i++) {
-                    if (offset + i >= document.length) { // we've gone of the end of the document
-                        return false;
-                    }
-                    if (document[offset + i] !== match[i]) { // this character doesn't match
-                        return false;
-                    }
-                }
-                return true;
-            };
-
-            if (inString) {
-                if (document[offset] === '\\') { // Java escape character
-                    offset++; // skip one character
-                }
-                else if (document[offset] === '"') {
-                    inString = false;
-                    const endIndex = offset + 1;
-                    this.contexts.push({
-                        type: ClojureContext.STRING,
-                        range: [stringStartIndex, endIndex]
-                    });
-                    stringStartIndex = null;
-                }
+            if (document[offset] === '\\') {
+                offset = this.findCharacterLiteralEnd(offset + 1);
             }
-            else {
-                if (document[offset] === '\\') {
-                    if (lookahead('u')) { // unicode character of the form \uNNNN
-                        offset += 4;
-                    }
-                    else if (lookahead('o')) { // octal character of the form \oNNN
-                        offset += 3;
-                    }
-                    else {
-                        let charCodeMatched = false;
-                        for (let charCode of CHARACTERS) { // match characters like \tab or \newline
-                            if (lookahead(charCode)) {
-                                offset += charCode.length;
-                                charCodeMatched = true;
-                                break;
-                            }
-                        }
-                        if (!charCodeMatched) { // this is just a single character
-                            offset++;
-                        }
-                    }
-                }
-                else if (document[offset] === '"') {
-                    inString = true;
-                    stringStartIndex = offset;
-                }
-                else if (document[offset] === ';') {
-                    const startPosition = offset;
-
-                    // seek ahead to the end of the line
-                    while (document[offset] !== '\n') {
-                        offset++;
-                    }
-
-                    const endPosition = offset + 1;
-                    this.contexts.push({
-                        type: ClojureContext.COMMENT,
-                        range: [startPosition, endPosition]
-                    });
-                }
+            else if (document[offset] === '"') {
+                const startIndex = offset;
+                offset = this.findStringEnd(offset + 1);
+                this.contexts.push({
+                    type: ClojureContext.STRING,
+                    range: [startIndex + 1, offset + 1],
+                });
+            }
+            else if (document[offset] === ';') {
+                const startIndex = offset;
+                offset = this.findCommentEnd(offset + 1);
+                this.contexts.push({
+                    type: ClojureContext.COMMENT,
+                    range: [startIndex + 1, offset + 1],
+                });
             }
         }
     }
@@ -107,10 +121,10 @@ export class ClojureContextResolver {
     // binary search for a context that contains this offset
     private findContext(offset: number, startIndex: number, endIndex: number): ClojureContext {
         const contains = (range: [number, number], i: number) => 
-            range[0] < i || i <= range[1];
+            range[0] <= i && i < range[1];
 
         const pivotIndex = Math.floor((endIndex - startIndex) / 2) + startIndex;
-        if (startIndex === endIndex) { // base case
+        if (startIndex >= endIndex) { // base case
             return ClojureContext.OTHER;
         }
         else if (contains(this.contexts[pivotIndex].range, offset)) {
@@ -119,8 +133,8 @@ export class ClojureContextResolver {
         else if (offset < this.contexts[pivotIndex].range[0]) {
             return this.findContext(offset, startIndex, pivotIndex);
         }
-        else if (offset >= this.contexts[pivotIndex].range[0]) {
-            return this.findContext(offset, pivotIndex, endIndex);
+        else if (offset >= this.contexts[pivotIndex].range[1]) {
+            return this.findContext(offset, pivotIndex + 1, endIndex);
         }
         console.error('Context search function exceeded the bounds of the document.');
         return ClojureContext.OTHER;
--- test/clojureContext.test.ts
+++ test/clojureContext.test.ts
@@ -6,13 +6,17 @@ suite('clojureContext', () => {
         [ClojureContext.OTHER, '', 0],
         [ClojureContext.OTHER, '(ns foo)', 2],
         [ClojureContext.STRING, '(ns "foo")', 5],
-        [ClojureContext.STRING, '(ns "foo")', 8],
+        [ClojureContext.STRING, '(ns "f\\"oo")', 8],
+        [ClojureContext.STRING, '(ns \\; "foo")', 11],
+        [ClojureContext.STRING, '(ns "f;oo")', 8],
         [ClojureContext.OTHER, '(ns "foo")', 4],
         [ClojureContext.OTHER, '(ns "foo")', 9],
         [ClojureContext.OTHER, '(ns "foo");(println "bar")\n(ns baz)', 10],
         [ClojureContext.COMMENT, '(ns "foo");(println "bar")\n(ns baz)', 26],
         [ClojureContext.OTHER, '(ns "foo");(println "bar")\n(ns baz)', 27],
         [ClojureContext.OTHER, '(ns "foo");(println "bar")\n(ns baz)', 1],
+        [ClojureContext.OTHER, '(ns \\;);(println "bar")\n(ns baz)', 7],
+        [ClojureContext.OTHER, '(ns \\");(println "bar")\n(ns baz)', 7],
     ];
     for (let [want, document, offset] of cases) {
         test(`new ClojureContextResolver(${document}).getContext("${offset}") should be "${want}"`, () => {
